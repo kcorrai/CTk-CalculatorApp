e2eadcd1c01de01fd4d3cc6a806800b0eedb23c7
kcorrai <yanlizcakaan@gmail.com>
2024-08-13 18:41:07 +0300

    calculator logic completed

--- calculator.py
+++ calculator.py
@@ -24,6 +24,8 @@ class Calculator(ctk.CTk):
         #data
         self.result_string = ctk.StringVar(value='0')
         self.formula_string = ctk.StringVar(value='0')
+        self.display_nums = []
+        self.full_operation = []
         
         #widgets
         self.create_widgets()
@@ -55,7 +57,69 @@ class Calculator(ctk.CTk):
                 MathImageButton(self, text='', func=self.math_press, operator=operator, col=data['col'], row=data['row'], font=main_font, image=divide_image)
             else:
                 MathButton(self, text=data['character'], operator=operator, func=self.math_press, col=data['col'], row=data['row'], font=main_font, span=1)
+        
+    def num_press(self, value):
+        self.display_nums.append(str(value))
+        full_number = ''.join(self.display_nums)
+        self.result_string.set(full_number)
+    
+    def math_press(self, value):
+        current_number = ''.join(self.display_nums)
+        if current_number:
+            self.full_operation.append(current_number)
+            if value != '=':
+                # update data
+                self.full_operation.append(value)
+                self.display_nums.clear()
+                
+                # update output
+                self.result_string.set('')
+                self.formula_string.set(' '.join(self.full_operation))
+            else:
+                formula = ' '.join(self.full_operation)
+                result = eval(formula)
+                
+                if isinstance(result, float):
+                    if result.is_integer():
+                        result = int(result)
+                    else:
+                        result = round(result, 3)
+                
+                # update data
+                self.full_operation.clear()
+                self.display_nums = [str(result)]
+                
+                # update output
+                self.result_string.set(result)
+                self.formula_string.set(formula)
+    
+    def clear(self):
+        # clear output
+        self.result_string.set(0)
+        self.formula_string.set('')
+        # clear data
+        self.display_nums.clear()
+        self.full_operation.clear()
+        
+    def percent(self):
+        if self.display_nums:
+            current_number = float(''.join(self.display_nums))
+            percent_number = current_number / 100
             
+            self.display_nums = list(str(percent_number))
+            self.result_string.set(''.join(self.display_nums))
+    
+    def invert(self):
+        current_number = ''.join(self.display_nums)
+        if current_number:
+        # pozitif/negatif
+            if float(current_number) > 0:
+                self.display_nums.insert(0, '-')
+            else:
+                del self.display_nums[0]
+            
+        self.result_string.set(''.join(self.display_nums))
+        
     def title_bar_color(self, is_dark):
         try:
             HWND = windll.user32.GetParent(self.winfo_id())
@@ -64,21 +128,6 @@ class Calculator(ctk.CTk):
             windll.dwmapi.DwmSetWindowAttribute(HWND, DWMWA_ATTRIBUTE, byref(c_int(COLOR)), sizeof(c_int))
         except:
             pass
-        
-    def num_press(self, value):
-        print(value)
-    
-    def math_press(self, value):
-        print(value)
-    
-    def clear(self):
-        print('clear')
-        
-    def percent(self):
-        print('percent')
-    
-    def invert(self):
-        print('invert')
             
 class OutputLabel(ctk.CTkLabel):
     def __init__(self, parent, row, anchor, font, string_var):
